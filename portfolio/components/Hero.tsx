4fbcb50a183be23bf32204f1f83e47c604bc80b0
AishJoshi-2002 <aishjoshi2002@gmail.com>
2025-04-10 23:15:39 +0530

    portfolio

---
+++ portfolio/components/Hero.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
+
+Ador
+
+
+.NET
+
+CLR (common language runtime)
+FCL(Framework class library)
+ASP .net
+
+.net
+sql
+indexing, keys, normalization ,db optimization
+
+
+react working, optimizations
+
+
+
+
+
+
